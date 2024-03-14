5333a3a335d220300bb12b83487822506504e60d
DhanakTwinkle <dhanak@usc.edu>
2024-03-13 20:39:53 -0700

    MinStack
    MinStack

---
+++ Medium/155.MinStack.java
@@ -0,0 +1,40 @@
+class MinStack {
+
+    Stack<Integer> stack;
+    int minVal = Integer.MAX_VALUE;
+
+    public MinStack() {
+        stack = new Stack<Integer>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if (minVal > val)
+        minVal = val;
+    }
+    
+    public void pop() {
+        if(!stack.isEmpty())
+        stack.pop();
+    }
+    
+    public int top() {
+        if(!stack.isEmpty())
+        return stack.peek();
+        else
+        return -1;
+    }
+    
+    public int getMin() {
+        return minVal;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */

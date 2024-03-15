2978ae4795f078182e98012c17b256d73679d569
DhanakTwinkle <dhanak@usc.edu>
2024-03-14 20:49:13 -0700

    BinaryTree - Preorder traversal - Using DFS and LL
    BinaryTree - Preorder traversal - Using DFS and LL

---
+++ Easy/144.BinaryTree-PreorderTraversal.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        // Note the LL used here
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        LinkedList<Integer> output = new LinkedList<>();
+
+
+        // IMP
+        if (root == null) {
+            return output;
+        }
+
+        stack.add(root); // In pre-order, we explore root first
+        while(!stack.isEmpty()){
+            // Process the top of stack first
+            TreeNode node = stack.pollLast();
+            output.add(node.val);
+
+            // Explore left and right
+            if(node.left!=null)
+                stack.add(node.left);
+            if(node.right!=null)
+                stack.add(node.right);
+        }
+        return output; 
+    }
+}

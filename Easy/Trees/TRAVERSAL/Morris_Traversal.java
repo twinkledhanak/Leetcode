3da80e6abc1fa3a108b055c96591c8fabc91023b
DhanakTwinkle <twinkledhanak@gmail.com>
2024-06-21 17:53:30 -0700

    Updated Morris code with comments
    Updated Morris code with comments

--- Easy/Trees/TRAVERSAL/Morris_Traversal.java
+++ Easy/Trees/TRAVERSAL/Morris_Traversal.java
@@ -4,22 +4,21 @@ class Solution {
 
         TreeNode node = root;
         while (node != null) {
-            if (node.left == null) {
+            if (node.left == null) { // if left doesnt exists, go to right directly.
                 output.add(node.val);
                 node = node.right;
-            } else {
-                TreeNode predecessor = node.left;
-                while (
-                    (predecessor.right != null) && (predecessor.right != node)
-                ) {
+            } 
+            else {
+                TreeNode predecessor = node.left; // Go to left and then go rightmost
+                while ((predecessor.right != null) && (predecessor.right != node)){
                     predecessor = predecessor.right;
                 }
 
-                if (predecessor.right == null) {
+                if (predecessor.right == null) { // if rightmost is null, save & go to left
                     output.add(node.val);
                     predecessor.right = node;
                     node = node.left;
-                } else {
+                } else { // rightmost is present, go to right
                     predecessor.right = null;
                     node = node.right;
                 }

7b3c17f19f36615d2b138d0c358cb96e707e1ffa
DhanakTwinkle <twinkledhanak@gmail.com>
2024-06-24 20:04:05 -0700

    BFS + DFS combination
    BFS + DFS combination

---
+++ Medium/Graphs/934.ShortestBridge.java
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/shortest-bridge/description/
+// Leetcode explanation is confusing
+// Refer to Neetcode for solution
+// https://www.youtube.com/watch?v=gkINMhbbIbU
+
+
+// https://leetcode.com/discuss/study-guide/1833581/bfs-and-its-variations

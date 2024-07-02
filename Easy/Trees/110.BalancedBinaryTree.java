f124fc8b77adc706491fc3c8010aadb2b22d3cdf
DhanakTwinkle <twinkledhanak@gmail.com>
2024-07-01 19:14:20 -0700

    New way of calculating height for balanced bTree
    New way of calculating height for balanced bTree

--- Easy/Trees/110.BalancedBinaryTree.java
+++ Easy/Trees/110.BalancedBinaryTree.java
@@ -1,6 +1,6 @@
 class Solution {
   public boolean isBalanced(TreeNode root) {
-    // An empty tree satisfies the definition of a balanced tree
+    // An empty tree (wow) satisfies the definition of a balanced tree
     if (root == null) {
       return true;
     }
@@ -22,3 +22,26 @@ class Solution {
   }
 
 };
+
+// What is the height considered here as? In terms of No of nodes or edges?
+// It is considered in terms of no of edges
+// Another alternative way of writing height() is:
+/*
+  public int height(TreeNode root){
+        // no of edges
+        if(root == null)
+            return 0;
+
+        if(root.left==null && root.right==null)
+            return 0;
+
+        return Math.max(height(root.left),height(root.right)) + 1;    
+
+    }
+ */
+
+
+// What I thought: 
+// Time: O(n) - we are traversing all n nodes. If it is balanced, h=logN. Can be both.
+// It is recursion within recursion, so for every node we are calling height
+// O(n * logN)
---
+++ Easy/Trees/111.Red-Black-AVL-Trees.java
@@ -0,0 +1,8 @@
+/*
+Applications of Red-Black Trees:
+Implementing maps and sets: Red-Black Trees are often used to implement maps and sets, where efficient search, insertion, and deletion are crucial.
+Priority queues: Red-Black Trees can be used to implement priority queues, where elements are ordered based on their priority.
+File systems: Red-Black Trees are used in some file systems to manage file and directory structures.
+In-memory databases: Red-Black Trees are sometimes used in in-memory databases to store and retrieve data efficiently.
+Graphics and game development: Red-Black Trees can be used in graphics and game development for tasks like collision detection and pathfinding.
+*/

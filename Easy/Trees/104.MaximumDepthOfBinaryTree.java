b172cb65f83fc4e8ff70a4504fa0bc015a31897d
DhanakTwinkle <twinkledhanak@gmail.com>
2024-07-01 19:13:11 -0700

    Added Depth/Height calculation by no. of nodes and edges
    Added Depth/Height calculation by no. of nodes and edges

--- Easy/Trees/104.MaximumDepthOfBinaryTree.java
+++ Easy/Trees/104.MaximumDepthOfBinaryTree.java
@@ -1,6 +1,8 @@
 class Solution {
+
+  // A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
   public int maxDepth(TreeNode root) {
-    if (root == null) {
+    if (root == null) { // we consider no of nodes, so return 0, not -1
       return 0;
     } 
     else {
@@ -12,3 +14,63 @@ class Solution {
 }
 
 // Time complexity: O(n)
+
+
+// Alternate code:
+/*
+class Solution {
+    public int maxDepth(TreeNode root) {
+        return helper(root);
+    }
+
+    public int helper(TreeNode root){
+        if(root == null) // no node, return no of nodes, which is 0
+            return 0;
+
+        if(root.left == null && root.right == null) // 1 node, return no of nodes, which is 1
+            return 1;
+
+        return Math.max(helper(root.left),helper(root.right)) + 1; // since no of nodes
+    }
+}
+
+*/
+
+/*
+         5
+      /      \
+   10       15
+  /   \      /   \
+20   25     30   35
+         \
+         45
+
+for root-> height is 3, depth(root): 0 (no of edges)
+for root-> height is 4, depth, 1 (no of nodes)
+
+    // consider no of nodes for Depth
+    public int maxDepthNodes(TreeNode root) {
+        if(root == null)
+            return 0; 
+        if(root.left == null & root.right == null)
+            return 1;
+        return Math.max(maxDepth(root.left),maxDepth(root.right)) + 1;
+            
+    }
+
+    // consider no of edges for Depth
+    public int maxDepthEdges(TreeNode root) {
+        if(root == null)
+            return 0;  
+        if(root.left == null & root.right == null)
+            return 0; // ******* only one base case difference
+        return Math.max(maxDepth(root.left),maxDepth(root.right)) + 1;
+            
+    }
+
+
+
+
+*/
+
+

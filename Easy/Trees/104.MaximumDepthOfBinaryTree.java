cea435473f5ab982e56c95da8c21d09075d9e85d
DhanakTwinkle <twinkledhanak@gmail.com>
2026-03-30 18:21:25 -0700

    Added Post Order version

--- Easy/Trees/104.MaximumDepthOfBinaryTree.java
+++ Easy/Trees/104.MaximumDepthOfBinaryTree.java
@@ -1,109 +1,63 @@
-class Solution {
-
-  // A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
-  public int maxDepth(TreeNode root) {
-    if (root == null) { // we consider no of nodes, so return 0, not -1
-      return 0;
-    } 
-    else {
-      int left_height = maxDepth(root.left);
-      int right_height = maxDepth(root.right);
-      return java.lang.Math.max(left_height, right_height) + 1;
-    }
-  }
-}
-
-// Time complexity: O(n)
-
-
-// Alternate code:
-public int maxDepth(TreeNode root) {
-        // No of nodes
-        // No of edges + 1
-
-        return helper(root,0);
-    }
-
-// No of nodes
-    public int helper(TreeNode root, int sum){
-        if(root == null)
-            return 0;
-
-        if(root.left == null && root.right == null)
-            return 1;    
-
-        // I have to capture the value from recursion 
-        sum += 1;
-
-        int left = helper(root.left,sum);
-        int right = helper(root.right,sum);   
-
-        int maxDepth = Math.max(left,right)+1; 
-        return maxDepth;
-    }
-
+/*
 The benefit of having tail recursion is that for certain programming languages (e.g. C++) the compiler could optimize the 
 memory allocation of the call stack by reusing the same space for every recursive call, rather than creating the space for 
 each one. As a result, one could obtain the constant space complexity O(1) for the overhead of the recursive calls.
-
 Note that the optimization of tail recursion is not supported by Python or Java.
+*/
 
+// Considering depth definition in terms of no of nodes
+// Following Post order defn as every node has same return value 
+ class Solution {
+     public int maxDepth(TreeNode root) {
+        if(root == null)
+			return 0;
 
+		int left = maxDepth(root.left);
+		int right = maxDepth(root.right);
+
+		return Math.max(left,right)+1; // no of nodes
+     }
+  }
+/**
+Time: O(H) => In this case, it could be O(n)
+Space: O(H) => Depending if the tree is completely skewed: O(n) or O(logn)
+*/
+
+// Iterative version is much more ugly for Post order versions
+// We explicitly have to track depth using parallel stacks 
+// Do this only if we have risk of stack overflow for tree depths
 
 /*
-class Solution {
+Why is depths a LL and not a variable?
+Because we explore multiple tree paths and single variable 
+cannot keep track of everything. So we have a parallel stack.
+
+    class Solution {
     public int maxDepth(TreeNode root) {
-        return helper(root);
-    }
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        LinkedList<Integer> depths = new LinkedList<>();
+        if (root == null) return 0;
 
-    public int helper(TreeNode root){
-        if(root == null) // no node, return no of nodes, which is 0
-            return 0;
+        stack.add(root);
+        depths.add(1);
 
-        if(root.left == null && root.right == null) // 1 node, return no of nodes, which is 1
-            return 1;
-
-        return Math.max(helper(root.left),helper(root.right)) + 1; // since no of nodes
+        int depth = 0, current_depth = 0;
+        while (!stack.isEmpty()) {
+            root = stack.pollLast();
+            current_depth = depths.pollLast();
+            if (root != null) {
+                depth = Math.max(depth, current_depth);
+                stack.add(root.left);
+                stack.add(root.right);
+                depths.add(current_depth + 1);
+                depths.add(current_depth + 1);
+            }
+        }
+        return depth;
     }
 }
 
 */
 
-/*
-         5
-      /      \
-   10       15
-  /   \      /   \
-20   25     30   35
-         \
-         45
-
-for root-> height is 3, depth(root): 0 (no of edges)
-for root-> height is 4, depth, 1 (no of nodes)
-
-    // consider no of nodes for Depth
-    public int maxDepthNodes(TreeNode root) {
-        if(root == null)
-            return 0; 
-        if(root.left == null & root.right == null)
-            return 1;
-        return Math.max(maxDepth(root.left),maxDepth(root.right)) + 1;
-            
-    }
-
-    // consider no of edges for Depth
-    public int maxDepthEdges(TreeNode root) {
-        if(root == null)
-            return 0;  
-        if(root.left == null & root.right == null)
-            return 0; // ******* only one base case difference
-        return Math.max(maxDepth(root.left),maxDepth(root.right)) + 1;
-            
-    }
-
-
-
-
-*/
 
 

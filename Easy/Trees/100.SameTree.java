5934ab2385a9a7c9142883bc11bfc2f02a2d3e68
DhanakTwinkle <twinkledhanak@gmail.com>
2026-04-30 15:26:08 -0700

    Added Iterative approach for Q100. SameTree

--- Easy/Trees/100.SameTree.java
+++ Easy/Trees/100.SameTree.java
@@ -28,3 +28,51 @@ class Solution {
         && isSameTree(p.right,q.right);        
     }
 }
+
+// Iterative approach
+// Comparing two trees side by side, BFS serves better than DFS
+class Solution {
+    public boolean check(TreeNode p, TreeNode q) {
+        // p and q are null
+        if (p == null && q == null) return true;
+        // one of p and q is null
+        if (q == null || p == null) return false;
+        if (p.val != q.val) return false;
+        return true;
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (!check(p, q)) return false;
+
+        // init deques
+        // BFS level order traversal. BFS? For early termination, comparing width is better.
+        // Level order traversal? Works best with BFS
+        ArrayDeque<TreeNode> deqP = new ArrayDeque<TreeNode>();
+        ArrayDeque<TreeNode> deqQ = new ArrayDeque<TreeNode>();
+        deqP.addLast(p);
+        deqQ.addLast(q);
+
+        while (!deqP.isEmpty()) {
+            // Since Queues, removeFirst(). If we wanted to use Stacks, then .removeLast()
+            p = deqP.removeFirst();
+            q = deqQ.removeFirst();
+
+            if (!check(p, q)) return false;
+            if (p != null) {
+                // in Java nulls are not allowed in Deque
+                if (!check(p.left, q.left)) return false;
+                if (p.left != null) {
+                    deqP.addLast(p.left);
+                    deqQ.addLast(q.left);
+                }
+                if (!check(p.right, q.right)) return false;
+                if (p.right != null) {
+                    deqP.addLast(p.right);
+                    deqQ.addLast(q.right);
+                }
+            }
+        }
+        return true;
+    }
+}
